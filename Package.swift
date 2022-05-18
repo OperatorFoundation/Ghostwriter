f78f9ffe74c9d0036eacbcda5650cbf9ca76e2b5
Unseelie <emunseelie@gmail.com>
2022-05-18 15:20:52 -0500

    updated to work on iOS

--- Package.swift
+++ Package.swift
@@ -1,11 +1,14 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Ghostwriter",
-    platforms: [.macOS(.v10_15)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
